09a1d40b900dbb0d0938555fe39ffcb08f25a690
Merijn <99054535@mydavinci.nl>
2020-01-08 14:31:53 +0100

    effe verplaatst
    :I

--- mastermind.js
+++ mastermind.js
@@ -7,4 +7,14 @@ var kleur = [
     "yellow"
 ];
 
-[Math.floor(Math.random() * kleur.length)]
+// [Math.floor(Math.random() * kleur.length)]
+
+var keuzeArray = [];
+for(var rij = 0; rij < 12; rij++) {
+    var tempKeuzeArray = document.createElement("div");
+    for(var plaatsInRij = 0; plaatsInRij < 4; plaatsInRij++) {
+        var obj = document.createElement("div");
+        tempKeuzeArray.appendChild(obj);
+    }
+    keuzeArray.push(tempKeuzeArray);
+}
