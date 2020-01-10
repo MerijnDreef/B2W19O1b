9b1b02c99f252715cb5d944af497ff0d694b64ba
Merijn <99054535@mydavinci.nl>
2020-01-10 10:16:19 +0100

    ik heb moeite met de pinnetjes
    :(

--- mastermind.js
+++ mastermind.js
@@ -11,12 +11,25 @@ var kleur = [
 
 var keuzeArray = [];
 for(var rij = 0; rij < 12; rij++) {
+    var combObject = document.createElement("div");
+     
+    var tempPinArray = document.createElement("div");
     var tempKeuzeArray = document.createElement("div");
+    for(var locatieInRij = 0; locatieInRij < 2; locatieInRij++){
+        var obj = document.createElement("div");
+        obj.className = "blokje"
+        var obj1 = document.createElement("div");
+        obj1.className = "blokje1"
+        tempPinArray.appendChild(obj);
+        tempPinArray.appendChild(obj1);
+    }
     for(var plaatsInRij = 0; plaatsInRij < 4; plaatsInRij++) {
         var obj = document.createElement("div");
         obj.className = "rondje"
         tempKeuzeArray.appendChild(obj);
     }
-    document.body.appendChild(tempKeuzeArray)
-    keuzeArray.push(tempKeuzeArray);
+    combObject.appendChild(tempPinArray);
+    combObject.appendChild(tempKeuzeArray);
+    document.body.appendChild(combObject);
+    keuzeArray.push(combObject);
 }
